2bf2664224ba09dab82f83960b456e08eb33de16
Andrey Tyukachev <tyukachev2014@mail.ru>
2024-05-10 21:54:25 +0300

    added slide closing

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const slides = document.querySelectorAll('.slide')
+const slides = document.querySelectorAll('.slide');
 
 for (const slide of slides) {
     slide.addEventListener('click', () => {
-        clearActiveClasses()
-
-        slide.classList.add('active')
-    })
+        if (slide.classList.contains('active')) {
+            slide.classList.remove('active');
+        }
+        else {
+            clearActiveClasses();
+            slide.classList.add('active');
+        }
+    });
 }
 
 function clearActiveClasses() {
-    slides.forEach((slide) =>{
-        slide.classList.remove('active')
-    })
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
 }
